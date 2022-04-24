ed3c909f3b03128919c031358e14f089b226d868
Precious <pkayode5@gmail.com>
2022-04-24 07:40:18 -0700

    this function compare two strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,4 +16,5 @@ void print_to_98(int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 #endif

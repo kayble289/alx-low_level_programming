ced6360c147b3a4f89bd6c6869f837fae87458af
Precious <pkayode5@gmail.com>
2022-04-24 07:34:16 -0700

    this function copies a string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,4 +15,5 @@ int add(int, int);
 void print_to_98(int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif
